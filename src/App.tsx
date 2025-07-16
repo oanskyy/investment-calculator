e58f78d4fbb02d0121eef370ca4205cab924b1c2
oanskyy <oana.anghelachi@gmail.com>
2025-07-16 21:30:26 +0100

    style(layout): make footer stick to bottom of screen on short pages

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,33 @@ import Header from "./components/Header"
 import InvestmentForm from "./components/InvestmentForm"
 import ResultCard from "./components/ResultCard"
 
+export type YearlyResult = {
+	year: number
+	interest: number
+	valueEndOfYear: number
+	totalInterest: number
+	investedCapital: number
+}
+
 function App() {
-	const [result, setResult] = useState<null | number>(null)
+	const [result, setResult] = useState<YearlyResult[] | null>(null)
 
 	return (
-		<>
+		<div className='flex flex-col min-h-screen bg-gradient-to-br from-green-200 via-green-100 to-green-300'>
 			<Header />
-			<main>
+			<main className='flex flex-col items-center flex-grow px-4 py-12 '>
 				<InvestmentForm onResult={setResult} />
-				{result !== null && <ResultCard value={result} />}
+				{result && <ResultCard value={result} />}
 			</main>
-			<footer></footer>
-		</>
+
+			{/* <pre>{JSON.stringify(result, null, 2)}</pre> */}
+
+			<footer>
+				<div className='text-center text-gray-600 py-8'>
+					<p>© {new Date().getFullYear()} Investment Calculator by @oansky</p>
+				</div>
+			</footer>
+		</div>
 	)
 }
 
